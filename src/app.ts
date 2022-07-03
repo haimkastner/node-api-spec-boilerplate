8c68e6da5c691c2cb390036694469e1630906576
Haim Kastner <haim.kastner@gmail.com>
2022-07-03 22:35:16 +0300

    Init project

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { RegisterRoutes } from "./generated/routes";
+import * as swaggerUi from 'swagger-ui-express';
+
+export const app = express();
+
+// Use body parser to read sent json payloads
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+RegisterRoutes(app);
+
+app.use('/', swaggerUi.serve, async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    return res.send(swaggerUi.generateHTML(await import('./generated/swagger.json')));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
---
+++ src/controllers/ping.controller.ts
@@ -0,0 +1,44 @@
+import {
+    Body,
+    Controller,
+    Get,
+    Path,
+    Post,
+    Query,
+    Route,
+    SuccessResponse,
+    Tags,
+} from "tsoa";
+
+interface Ping {
+    /** Who is the request caller (free text) */
+    whois: string;
+}
+
+interface Pong {
+    /** The greent arrived from API caller */
+    greeting: string;
+    /** The tome when the ping request arrived */
+    time: number;
+}
+
+@Tags('Status')
+@Route("Status")
+export class PingController extends Controller {
+
+    /**
+     * Send Ping request to API server.
+     * @param greeting The greeting to send :)
+     * @param ping The ping payload
+     * @returns A Pong object
+     */
+    @Post()
+    public async ping(@Query() greeting: string, @Body() ping?: Ping): Promise<Pong> {
+
+        console.log(`New ping arrived from "${ping?.whois}" who greet us with "${greeting}" :)`);
+        return {
+            greeting,
+            time: new Date().getTime(),
+        };
+    }
+}
